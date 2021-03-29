8b9cbde371b5accb8914d771970e4fd94517fa3f
Lukas_Schaub <lukas_schaub@gmx.ch>
2021-03-29 16:51:31 +0200

    added both stl in one figure

--- Matching_3D_Models.m
+++ Matching_3D_Models.m
@@ -1,14 +1,24 @@
 %% Setup reading files and creating point clouds
+clear;clc;close all;
 
-clear;clc;close;
-
+% reading stl files
 stlData = stlread('Mand-left-cut.stl');
 mand = stlData.Points;
 stlData1 = stlread('Pelvis-left-cut.stl');
 pelvis = stlData1.Points;
 
-%Initial Overview 3D plots of both stl objects
+% Initial Overview 3D plots of both stl objects sepeartle
+figure
 mand_fig = plot3(mand(:,1),mand(:,2),mand(:,3),'.');
-pelvis_fig = plot3(pelvis(:,1),pelvis(:,2),pelvis(:,3),'.');
+title('mand')
 
-% optimization magic
+figure
+pelvis_fig = plot3(pelvis(:,1),pelvis(:,2),pelvis(:,3),'.');
+title('pelvis')
+
+%plot of both in one figure
+figure
+title('mixed')
+plot3(mand(:,1),mand(:,2),mand(:,3),'.')
+hold on
+pelvis_fig = plot3(pelvis(:,1),pelvis(:,2),pelvis(:,3),'k.');
