bb55a64dbb95d2e4af0248f998dc7ab00cf7e2e9
ClaIgl <74904581+ClaIgl@users.noreply.github.com>
2021-04-08 17:21:12 +0200

    Add files via upload

---
+++ directed_hausdorff_distance.m
@@ -0,0 +1,27 @@
+function [dhd] = directed_hausdorff_distance(X,Y)
+%Calculates the directed hausdorff distance for X to Y
+%   If the directed Hausdorff distance is zero all points of X lie on a
+%   point in Y, the converse might not be true
+
+dimX = size(X);
+dimY = size(Y);
+%check if the two sets have the same dimension
+if dimX(2) ~= dimY(2)
+    fprintf('All points must have the same dimension')
+end
+
+%calculate the directed distance from X to Y
+dXY_all = zeros(1, dimX(1));
+for i=1:dimX(1)
+    shortestdist = norm(X(i,:) - Y(1,:));
+    for j=2:dimY(1)
+        dist = norm(X(i,:) - Y(j,:));
+        if dist < shortestdist
+            shortestdist = dist;
+        end
+    end
+    dXY_all(i) = shortestdist;
+end
+dhd = max(dXY_all);
+end
+
